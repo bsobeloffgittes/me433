5a7e5c5d725446ca5ea7784f56e947c01a438bbe
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-04-04 23:53:20 -0500

    cleaned up hw2 code

--- HW2/HW2/HW2.c
+++ HW2/HW2/HW2.c
@@ -6,20 +6,15 @@
 #define BUTTON_PIN 16
 
 
-static char event_str[128];
 
 uint16_t num_presses;
 
-void gpio_event_string(char *buf, uint32_t events);
-
 void gpio_callback(uint gpio, uint32_t events) {
-    // Put the GPIO event(s) that just happened into event_str
-    // so we can print it
+    // Do what needs to happen when button pressed
     gpio_put(LED_PIN, (++num_presses)%2);
     printf("Number of presses: %d\r\n", num_presses);
 
 }
-
 
 // Perform initialisation
 int pico_led_init(void) {
@@ -29,13 +24,6 @@ int pico_led_init(void) {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     return PICO_OK;
 }
-
-// // Turn the led on or off
-// void pico_set_led(bool led_on) {
-//     // Just set the GPIO on or off
-//     gpio_put(LED_PIN, led_on);
-    
-// }
 
 int main()
 {
@@ -55,37 +43,3 @@ int main()
 
     while (1);
 }
-
-
-
-
-
-
-// // Extra stuff for interrupt
-// static const char *gpio_irq_str[] = {
-//     "LEVEL_LOW",  // 0x1
-//     "LEVEL_HIGH", // 0x2
-//     "EDGE_FALL",  // 0x4
-//     "EDGE_RISE"   // 0x8
-// };
-
-// void gpio_event_string(char *buf, uint32_t events) {
-// for (uint i = 0; i < 4; i++) {
-//     uint mask = (1 << i);
-//     if (events & mask) {
-//         // Copy this event string into the user string
-//         const char *event_str = gpio_irq_str[i];
-//         while (*event_str != '\0') {
-//             *buf++ = *event_str++;
-//         }
-//         events &= ~mask;
-
-//         // If more events add ", "
-//         if (events) {
-//             *buf++ = ',';
-//             *buf++ = ' ';
-//         }
-//     }
-// }
-// *buf++ = '\0';
-// }
