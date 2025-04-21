f30a1e1149a727ec6f6b5a2c8ac82d94fadeea92
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-04-21 11:37:01 -0500

    made code to count clock cycles

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -2,35 +2,55 @@
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
 
 
+int main() {
 
-int main()
-{
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+    while(!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+
+    volatile float f1, f2;
+    printf("Enter two floats to use:");
+    scanf("%f %f", &f1, &f2);
+    volatile float f_add, f_sub, f_mult, f_div;
+
+    absolute_time_t t1 = get_absolute_time();
+    for(int i = 0; i < 1000; i++) {
+        f_add = f1 + f2;
+    }
+    absolute_time_t t2 = get_absolute_time();
+    uint64_t t_add = (to_us_since_boot(t2) - to_us_since_boot(t1))/(6.667);
+
+    t1 = get_absolute_time();
+    for(int i = 0; i < 1000; i++) {
+        f_sub = f1 - f2;
+    }
+    t2 = get_absolute_time();
+    uint64_t t_sub = (to_us_since_boot(t2) - to_us_since_boot(t1))/(6.667);
+
+    t1 = get_absolute_time();
+    for(int i = 0; i < 1000; i++) {
+        f_mult = f1 * f2;
+    }
+    t2 = get_absolute_time();
+    uint64_t t_mult = (to_us_since_boot(t2) - to_us_since_boot(t1))/(6.667);
+
+    t1 = get_absolute_time();
+    for(int i = 0; i < 1000; i++) {
+        f_div = f1 / f2;
+    }
+    t2 = get_absolute_time();
+    uint64_t t_div = (to_us_since_boot(t2) - to_us_since_boot(t1))/(6.667);
+
+    printf("\nResults: \n%f+%f=%f \n%f-%f=%f \n%f*%f=%f \n%f/%f=%f\n", f1,f2,f_add, f1,f2,f_sub, f1,f2,f_mult, f1,f2,f_div);
+
+    printf("\nClock cycles:\nAddition: %llu\nSubtraction: %llu\nMultiplication: %llu\nDivision: %llu", t_add, t_sub, t_mult, t_div);
 
     while (true) {
-        printf("Hello, world!\n");
+        //printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
