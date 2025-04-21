c6c92c0c1699b43537cb9151616ff850ad124f98
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-04-21 13:04:18 -0500

    added initial spi framework

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <math.h>
+
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS   17
+#define PIN_SCK  18
+#define PIN_MOSI 19
+
+#define DATA_A_MASK 0b00110000
+#define DATA_B_MASK 0b10110000
+
+
+static inline void cs_select(uint cs_pin);
+static inline void cs_deselect(uint cs_pin);
+
+void pack_buffer(uint8_t* buffer, uint8_t mask, uint16_t num);
 
 void count_cycles(void);
 
@@ -8,17 +28,63 @@ int main() {
 
     stdio_init_all();
 
-    while(!stdio_usb_connected()) {
-        sleep_ms(100);
-    }
+    // SPI initialisation. This example will use SPI at 1MHz.
+    spi_init(SPI_PORT, 1000*1000);
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
+    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    
+    // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+
+    // while(!stdio_usb_connected()) {
+    //     sleep_ms(100);
+    // }
 
     // Perform the clock cycle counting part of the assignment
-    count_cycles();
+    // count_cycles();
 
     while (true) {
         sleep_ms(1000);
     }
 }
+
+
+
+
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+
+void pack_buffer(uint8_t* buffer, uint8_t mask, uint16_t num) {
+    int new_num = num & 0x3FF;
+    buffer[0] = mask | new_num >> 6;
+    buffer[1] = (new_num && 0x3F) << 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
