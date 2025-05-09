7d3276802b5cf5d88f7584a17a42a98387b023ce
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-05-09 12:03:23 -0500

    pixel blinking on screen for hw7

---
+++ HW7/HW7.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "ssd1306.h"
+#include "hardware/i2c.h"
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+#define ONBOARD_LED 25
+
+void heartbeat(void);
+
+
+int main()
+{
+    stdio_init_all();
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    //gpio_pull_up(I2C_SDA);
+    //gpio_pull_up(I2C_SCL);
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    ssd1306_setup();
+    sleep_ms(100);
+    // ssd1306_clear();
+
+
+
+    gpio_init(ONBOARD_LED);
+    gpio_set_dir(ONBOARD_LED, GPIO_OUT);
+
+
+
+
+    while (true) {
+        heartbeat();
+        sleep_ms(5);
+    }
+}
+
+
+
+
+
+
+
+void heartbeat() {
+    static uint16_t i = 0;
+    i = (i+1)%100;
+    if(i==0) {
+        gpio_put(ONBOARD_LED, 0);
+        ssd1306_drawPixel(0,0,1);
+    }
+    else if(i==50) {
+        // ssd1306_drawPixel(0,0,0);
+        ssd1306_clear();
+        gpio_put(ONBOARD_LED, 1);
+    }
+    ssd1306_update();
+}
