1d0dd9d63cb28e174a7a3141e896ec30b272ef0a
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-05-09 13:17:06 -0500

    hw7 finished

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -3,6 +3,7 @@
 #include "ssd1306.h"
 #include "hardware/i2c.h"
 #include "font.h"
+#include "hardware/adc.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -27,25 +28,51 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    //gpio_pull_up(I2C_SDA);
-    //gpio_pull_up(I2C_SCL);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     ssd1306_setup();
     sleep_ms(100);
     // ssd1306_clear();
 
+    // Initialize ADC
+    adc_init();
+    adc_gpio_init(26); // ADC0 pin is used as ADC
+    adc_select_input(0); // select to read from ADC0
+
 
 
     gpio_init(ONBOARD_LED);
     gpio_set_dir(ONBOARD_LED, GPIO_OUT);
 
+    unsigned int past_t = 0;
+    unsigned int t;
 
+    char fps_msg[10];
+    char message[50];
 
 
     while (true) {
         heartbeat();
-        sleep_ms(5);
+        // sleep_ms(5);
+        // ssd1306_drawPixel(0,0,1);
+        // draw_letter('A', 10, 5);
+
+        t = to_us_since_boot(get_absolute_time());
+
+        sprintf(fps_msg, "fps: %d", 1000000 / (t - past_t));
+
+        past_t = t;
+
+        draw_message(fps_msg, 17*5, 25);
+
+        uint16_t adc_val = adc_read();
+        sprintf(message, "ADC: %d", adc_val);
+        draw_message(message, 10, 10);
+
+        ssd1306_update();
+
     }
 }
 
@@ -60,19 +87,16 @@ void heartbeat() {
     i = (i+1)%100;
     if(i==0) {
         gpio_put(ONBOARD_LED, 0);
-        ssd1306_drawPixel(0,0,1);
-        // draw_letter('A', 10, 5);
-        char message[50];
-        sprintf(message, "hello world");
-        draw_message(message, 40, 15);
     }
     else if(i==50) {
         // ssd1306_drawPixel(0,0,0);
-        ssd1306_clear();
+        // ssd1306_clear();
         gpio_put(ONBOARD_LED, 1);
     }
-    ssd1306_update();
 }
+
+
+
 
 
 void draw_letter(char c, uint8_t x, uint8_t y) {
@@ -93,5 +117,4 @@ void draw_message(char* c, uint8_t x, uint8_t y) {
 
         draw_letter(next_char, x + 6*i, y);
     }
-    ssd1306_update();
 }
