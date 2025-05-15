2659dc5d055768c1eea769dbb69b920645a50fc0
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-05-15 14:46:15 -0500

    redid maf plots

--- HW10/hw10.py
+++ HW10/hw10.py
@@ -6,7 +6,7 @@ import csv
 t = [] # column 0
 data1 = [] # column 1
 
-filename = 'sigD.csv'
+filename = 'sigA.csv'
 
 with open(filename) as f:
     # open the csv file
@@ -32,74 +32,17 @@ with open(filename) as f:
 
 # MOVING AVERAGE FILTER ------------------------------------------------------------
 
-# num_average = 500
+num_average = 1000
 
-# filtered_arr = []
-
-
-
-# for i in range(len(t) - num_average):
-#     # print the data to verify it was read
-#     filtered_arr.append(sum(data1[i:(i+num_average)])/num_average)
+filtered_arr = []
 
 
 
-
-
-# Ts = (t[-1] - t[0]) / len(t) # sampling interval
-# Fs = 1.0 / Ts # sample rate
-# ts = np.arange(0,t[-1],Ts) # time vector
-# y = data1 # the data to make the fft from
-# y_filt = filtered_arr
-
-
-# n = len(y) # length of the 
-# n_filt = len(y_filt)
-
-# k = np.arange(n)
-# k_filt = np.arange(n_filt)
-
-# T = n/Fs
-# frq = k/T # two sides frequency range
-# frq = frq[range(int(n/2))] # one side frequency range
-
-# frq_filt = k/T # two sides frequency range
-# frq_filt = frq_filt[range(int(n_filt/2))] # one side frequency range
-
-# Y = np.fft.fft(y)/n # fft computing and normalization
-# Y = Y[range(int(n/2))]
-
-# Y_filt = np.fft.fft(y_filt)/n_filt # fft computing and normalization
-# Y_filt = Y_filt[range(int(n_filt/2))]
-
-# fig, (ax1, ax2) = plt.subplots(2, 1)
-# ax1.plot(t,y,'b')
-# ax1.plot(t[0:len(y_filt)],y_filt,'r')
-
-# ax1.set_xlabel('Time')
-# ax1.set_ylabel('Amplitude')
-
-
-# ax2.loglog(frq,abs(Y),'b') # plotting the fft
-
-# ax2.loglog(frq_filt,abs(Y_filt),'r') # plotting the fft
-
-# ax2.set_xlabel('Freq (Hz)')
-# ax2.set_ylabel('|Y(freq)|')
-# plt.show()
-
-# IIR --------------------------------------------------------
-
-A = 0.003
-B = 0.997
-
-filtered_arr = [data1[0]]
-
-avg = 0
-
-for i in range(len(t) - 1):
+for i in range(len(t) - num_average):
     # print the data to verify it was read
-    filtered_arr.append(A * data1[i+1] + B * filtered_arr[-1])
+    filtered_arr.append(sum(data1[i:(i+num_average)])/num_average)
+
+
 
 
 
@@ -144,5 +87,65 @@ ax2.loglog(frq_filt,abs(Y_filt),'r') # plotting the fft
 ax2.set_xlabel('Freq (Hz)')
 ax2.set_ylabel('|Y(freq)|')
 
-fig.suptitle(f'{filename} IIR Plot: A = {A}, B = {B}')
+fig.suptitle(f'{filename} MAF: {num_average} samples averaged')
+
 plt.show()
+
+# IIR --------------------------------------------------------
+
+# A = 0.003
+# B = 0.997
+
+# filtered_arr = [data1[0]]
+
+# avg = 0
+
+# for i in range(len(t) - 1):
+#     # print the data to verify it was read
+#     filtered_arr.append(A * data1[i+1] + B * filtered_arr[-1])
+
+
+
+# Ts = (t[-1] - t[0]) / len(t) # sampling interval
+# Fs = 1.0 / Ts # sample rate
+# ts = np.arange(0,t[-1],Ts) # time vector
+# y = data1 # the data to make the fft from
+# y_filt = filtered_arr
+
+
+# n = len(y) # length of the 
+# n_filt = len(y_filt)
+
+# k = np.arange(n)
+# k_filt = np.arange(n_filt)
+
+# T = n/Fs
+# frq = k/T # two sides frequency range
+# frq = frq[range(int(n/2))] # one side frequency range
+
+# frq_filt = k/T # two sides frequency range
+# frq_filt = frq_filt[range(int(n_filt/2))] # one side frequency range
+
+# Y = np.fft.fft(y)/n # fft computing and normalization
+# Y = Y[range(int(n/2))]
+
+# Y_filt = np.fft.fft(y_filt)/n_filt # fft computing and normalization
+# Y_filt = Y_filt[range(int(n_filt/2))]
+
+# fig, (ax1, ax2) = plt.subplots(2, 1)
+# ax1.plot(t,y,'b')
+# ax1.plot(t[0:len(y_filt)],y_filt,'r')
+
+# ax1.set_xlabel('Time')
+# ax1.set_ylabel('Amplitude')
+
+
+# ax2.loglog(frq,abs(Y),'b') # plotting the fft
+
+# ax2.loglog(frq_filt,abs(Y_filt),'r') # plotting the fft
+
+# ax2.set_xlabel('Freq (Hz)')
+# ax2.set_ylabel('|Y(freq)|')
+
+# fig.suptitle(f'{filename} IIR Plot: A = {A}, B = {B}')
+# plt.show()
