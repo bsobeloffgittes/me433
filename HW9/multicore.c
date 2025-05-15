2464135c00c1f55543ab8eb69879bcfb75efb34b
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-05-15 13:05:31 -0500

    hw9 working

--- HW9/multicore.c
+++ HW9/multicore.c
@@ -7,27 +7,72 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
-#define FLAG_VALUE 123
+#define LED_PIN 15
+#define ADC_PIN 26
+
+#define FLAG0 0
+#define FLAG1 1
+#define FLAG2 2
 
 void core1_entry() {
 
-    multicore_fifo_push_blocking(FLAG_VALUE);
 
-    uint32_t g = multicore_fifo_pop_blocking();
+    adc_init();
+    adc_gpio_init(ADC_PIN); // ADC0 pin is used as ADC
+    adc_select_input(0); // select to read from ADC0
 
-    if (g != FLAG_VALUE)
-        printf("Hmm, that's not right on core 1!\n");
-    else
-        printf("Its all gone well on core 1!");
+    // printf("in core1\n");
 
-    while (1)
-        tight_loop_contents();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // multicore_fifo_push_blocking(FLAG_VALUE);
+
+    // printf("pushed\n");
+
+    // uint32_t g = multicore_fifo_pop_blocking();
+
+    // if (g != FLAG_VALUE)
+    //     printf("Hmm, that's not right on core 1!\n");
+    // else
+    //     printf("Its all gone well on core 1!");
+
+    while (1) {
+        uint32_t g = multicore_fifo_pop_blocking();
+
+        char msg[50];
+
+        sleep_ms(100);
+
+        // sprintf(msg, "flag recieved: %d", g);
+        printf(msg);
+
+        if(g == FLAG0) {
+            uint32_t adc_val = (uint32_t) adc_read();
+            multicore_fifo_push_blocking((uint32_t) adc_val);
+        } else if (g == FLAG1) {
+            gpio_put(LED_PIN, true);
+        } else if (g == FLAG2) {
+            gpio_put(LED_PIN, false);
+        }
+    }
+        // tight_loop_contents();
 }
 
 int main() {
     stdio_init_all();
+
+
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+
+
     printf("Hello, multicore!\n");
+
 
     /// \tag::setup_multicore[]
 
@@ -35,13 +80,34 @@ int main() {
 
     // Wait for it to start up
 
-    uint32_t g = multicore_fifo_pop_blocking();
+    printf("Launched Core 1\n");
 
-    if (g != FLAG_VALUE)
-        printf("Hmm, that's not right on core 0!\n");
-    else {
-        multicore_fifo_push_blocking(FLAG_VALUE);
-        printf("It's all gone well on core 0!");
+    // uint32_t g = multicore_fifo_pop_blocking();
+
+    // if (g != FLAG_VALUE)
+    //     printf("Hmm, that's not right on core 0!\n");
+    // else {
+    //     multicore_fifo_push_blocking(FLAG_VALUE);
+    //     printf("It's all gone well on core 0!");
+    // }
+
+    while(1) {
+        char msg[50];
+        uint32_t flag_num;
+
+        scanf("%d", &flag_num);
+
+        sprintf(msg, "%d\n", flag_num);
+        printf(msg);
+
+        multicore_fifo_push_blocking(flag_num);
+
+        if(flag_num == FLAG0) {
+            // printf("flag0");
+            uint32_t g = multicore_fifo_pop_blocking();
+            sprintf(msg, "Voltage: %0.2f\n", (3.3 * ((float) g))/4095.0);
+            printf(msg);
+        }
     }
 
     /// \end::setup_multicore[]
